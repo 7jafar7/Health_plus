578867a92d7e35dd19399db2d489629097295b75
Jafar Alkozy <j7afa7r@gmail.com>
2024-11-24 16:29:35 +0200

    first commit

---
+++ lib/homepage.dart
@@ -0,0 +1,23 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home page'),
+        centerTitle: true,
+      ),
+      body: Container(),
+    );
+  }
+}
